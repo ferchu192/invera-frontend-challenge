92a9a0e03a26dcaf63f79f1a37965e242db05b2e
Fernando Gimenez <fergimenezreyes@gmail.com>
2026-01-18 10:01:07 -0300

    feat(table): add base styling, new SVGs, and reorganize svg assets

---
+++ client/components/Table/ActionColumn/index.tsx
@@ -0,0 +1,10 @@
+const ActionColumn = () => {
+  return (
+    <div className="flex items-center gap-[8px]">
+      <img src="public/table/edit.svg" alt="edit" className="w-[12px] h-[12px]" />
+      <img src="public/table/delete.svg" alt="delete" className="w-[12px] h-[12px]" />
+    </div>
+  )
+}
+
+export default ActionColumn;
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
-import MetricsLayout from '../layouts/Metrics';
+import Table from '../components/Table';
 
 function App() {
   return (
-    <div className="w-full">
-      <MetricsLayout />
+    <div className="w-full min-h-screen bg-bg-page">
+      <Table title="Table" columns={[]} />
     </div>
   )
 }
